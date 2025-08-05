c62f1cf84f11369cb1aaa600c4bc0c35ab241e33
SpiritAndPollen <jrtt212@gmail.com>
2025-08-06 09:08:11 +1000

    Initial Commit

---
+++ Hush SOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hush SOS
+//
+//  Created by Jedda Tuuta on 6/8/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hush SOS/Hush_SOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hush_SOSApp.swift
+//  Hush SOS
+//
+//  Created by Jedda Tuuta on 6/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hush_SOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
